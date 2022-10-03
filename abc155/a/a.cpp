88175e16ea9158a6d5d9bb77f8a9d316d8418bf0
naharagu <76852123+naharagu@users.noreply.github.com>
2022-10-03 22:10:20 +0200

    submit

---
+++ abc155/a/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for (int i = 1; i < (int)(n+1); i++)
+using ll = long long;
+using P = pair<int,int>;
+using Graph = vector<vector<int>>;
+using mint = modint1000000007;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	if ((a == b && b == c)|| (a != b && b != c && a != c))
+		cout << "No" << endl;
+	else
+		cout << "Yes" << endl;
+}
