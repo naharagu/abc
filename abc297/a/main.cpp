3938404c8a97712cbf21f87353829a1131fb4242
naharagu <76852123+naharagu@users.noreply.github.com>
2023-04-22 13:57:03 +0200

    submit

--- abc297/a/main.cpp
+++ abc297/a/main.cpp
@@ -4,14 +4,23 @@ using namespace std;
 using namespace atcoder;
 using ll = long long;
 
-int main()
-{
-	int n, d;
-	cin >> n >> d;
+int main() {
+    int n, d;
+    cin >> n >> d;
 
-	vector<int> t(n);
-	for (int i = 0; i < n; ++i)
-		cin >> t[i];
-	
-	int ans = -1;
+    vector<int> click_times(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> click_times[i];
+    }
+
+    int first_double_click = -1;
+    for (int i = 1; i < n; ++i) {
+        if (click_times[i] - click_times[i - 1] <= d) {
+            first_double_click = click_times[i];
+            break;
+        }
+    }
+
+    cout << first_double_click << endl;
+    return 0;
 }
